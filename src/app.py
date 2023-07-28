76f5d259f139741e3ac010e9ebe2e2f496ae9edc
Jose Morrone <josemmorrone@gmail.com>
2023-07-28 18:56:33 +0000

    [ADD] Agrego el metodo PUT para actualizar

--- src/app.py
+++ src/app.py
@@ -36,22 +36,15 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
-    return jsonify(response_body), 200
-
-@app.route("/contacts", methods=['GET', 'POST', 'DELETE'])
+@app.route("/contacts", methods=['GET', 'POST', 'DELETE', 'PUT'])
 def handle_contacts():
+    # METODO GET
     if request.method == 'GET':
         contacts = Contact.query.all()
         contacts = list(map(lambda contact: contact.serialize(), contacts))
         return jsonify(contacts), 200
 
+    #METODO POST
     if request.method == 'POST':
 
         body = request.json
@@ -59,6 +52,11 @@ def handle_contacts():
         full_name = body.get("full_name", None)
         phone_number = body.get("phone_number", None)
         adress = body.get("adress", None)
+
+        if email is None or full_name is None or phone_number is None or adress is None:
+            return jsonify("Todos los campos son requeridos"), 400
+        if email == "" or full_name == "":
+            return jsonify("El email y el correo no pueden estar vacios"), 400
 
         try:
             new_contact = Contact(
@@ -75,7 +73,38 @@ def handle_contacts():
 
             db.session.rollback()
             return jsonify(error.args), 500
-    
+
+    #METODO PUT
+    if request.method == 'PUT':
+        body = request.json
+        id = body.get('id', None)
+
+        if id == None:
+            return jsonify("Debe enviar el id del contacto a actaulizar"), 400
+        contact = Contact.query.filter_by(id = id).first()
+        if contact == None:
+            return jsonify("El contacto no existe"), 400
+
+        email = body.get('email', None)
+        full_name = body.get("full_name", None)
+        phone_number = body.get("phone_number", None)
+        adress = body.get("adress", None)
+        
+        if email is None or full_name is None or phone_number is None or adress is None:
+            return jsonify("Todos los campos son requeridos"), 400
+
+        try:
+            contact.email = email
+            contact.full_name = full_name
+            contact.phone_number = phone_number
+            contact.adress = adress
+            db.session.commit()
+            return jsonify("Contacto actualizado"), 200
+        except Exception as error:
+            db.session.rollback()
+            return jsonify(error.args), 500
+
+    #METODO DELETE
     if request.method == 'DELETE':
         body = request.json
         id = body.get('id', None)
@@ -83,8 +112,6 @@ def handle_contacts():
         db.session.delete(contact)
         db.session.commit()
         return "Contacto eliminado", 200
-
-    
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
